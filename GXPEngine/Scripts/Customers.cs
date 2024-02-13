37b128fe4e18a080bab59022f078118440b6b7ff
Dumbita <549149@student.saxion.nl>
2024-02-13 19:47:33 +0100

    polishing code to be more understandable

---
+++ GXPEngine/Scripts/Customers.cs
@@ -0,0 +1,40 @@
+﻿using GXPEngine;
+using System;
+
+public class Customers : AnimationSprite
+{
+    //the time it takes to change frames variables
+    int counter;
+    int frame;
+
+    public Customers() : base("Faces.png", 5, 2)
+    {
+
+    }
+
+    void Update()
+    {
+
+        //counter increase speed
+        counter++;
+        //change of sprite
+        if (counter > 200)
+        {
+
+            counter = 0;
+
+            frame++;
+            //restart
+            if (frame == frameCount)
+            {
+
+                frame = 0;
+            }
+
+            SetFrame(frame);
+
+        }
+
+    }
+
+}
