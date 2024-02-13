637ba830a1f554ddee6a6a5b33cf72d308ae67c2
Dumbita <549149@student.saxion.nl>
2024-02-13 10:46:38 +0100

    Testing intital commit

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -19,67 +19,8 @@ public class MyGame : Game {
 
 	public MyGame() : base(800, 600, false)     // Create a window that's 800x600 and NOT fullscreen
 	{
-		// Create a full screen canvas (EasyDraw):
-		// (in MyGame, width and height refer to game.width and game.height: the window size)
-		background = new EasyDraw(width, height);
 
-		FillBackground();
 
-		// Create a sprite from an image file (png, in bin/Debug):
-		spaceShip = new Sprite("playerShip1_blue.png");
-		// Set the (rotation) origin of the space ship to its center:
-		// (spaceShip.width and spaceShip.height refer to the width and height of the sprite, in pixels)
-		spaceShip.SetOrigin(spaceShip.width / 2, spaceShip.height / 2);
-
-		// Create an animation sprite from a sprite sheet (png file, in bin/Debug) with four columns and two rows:
-		character = new AnimationSprite("platformerPack_character.png", 4, 2);
-		// Set the origin of the character sprite in the top middle:
-		character.SetOrigin(character.width / 2, 0);
-
-		SetStartPositions();
-
-		// Create a small button canvas (EasyDraw):
-		button = new EasyDraw(100, 30);
-		// Clear the canvas with a grayscale value, semi-transparent:
-		// (The values are red, green, blue, alpha, all between 0 and 255)
-		button.Clear(100, 100, 100, 128);
-		// Write the text "RESET" on it (centered):
-		button.TextAlign(CenterMode.Center, CenterMode.Center);
-		button.Text("RESET");
-		button.SetOrigin(button.width / 2, button.height / 2);
-		// Set the position of the button to the bottom middle of the screen:
-		button.SetXY(width / 2, height - button.height);
-
-		// Create a larger canvas with some information:
-		information = new EasyDraw(400, 100);
-		// Set the text color to a blue-ish tint (red, green, blue values between 0 and 255):
-		information.Fill(150, 150, 255);
-		information.TextAlign(CenterMode.Min, CenterMode.Center);
-		information.Text(
-			"Use WASD to steer the space ship\n" +
-			"and arrows to control the character\n" +
-			"and sound track volume"
-		);
-		information.SetOrigin(0, information.height);
-		information.SetXY(0, 0.75f * height);
-
-		// Add all sprites to the engine, so that they will be displayed every frame:
-		// (The order that we add them is the order that they will be drawn.)
-		AddChild(background);
-
-		AddChild(character);
-		AddChild(spaceShip);
-
-		AddChild(button);
-		AddChild(information);
-
-		// Play a sound track, looping and streaming, and keep a reference to it such that
-		// we can change the volume:
-		// (The .ogg file is in bin/Debug. ogg, mp3 and wav files are supported)
-		soundTrack = new Sound("The_Endless_Journey.ogg", true, true).Play();
-
-		// Print some information to the console (behind the game window):
-		Console.WriteLine("Scene successfully initialized");
 	}
 
 	void FillBackground() {
