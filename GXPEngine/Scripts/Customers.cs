487b8f56a018788ad8b0d72be3833bbed08b4c9e
Damyan <d.peychev12@gmail.com>
2024-02-14 14:42:54 +0100

    changes

--- GXPEngine/Scripts/Customers.cs
+++ GXPEngine/Scripts/Customers.cs
@@ -10,41 +10,27 @@ public class Customers : AnimationSprite
     float lastChangeOfFace;
     float timer = 1000;
 
-    Random random = new Random();
+    Random random = new Random(); 
 
-    
     GameSettings settings;
 
-    public Customers(GameSettings settings) : base(settings.people[new Random().Next (1, 5)], 5, 2)
+    public Customers(GameSettings settings) : base(settings.people[new Random().Next(1, 5)], 5, 2)
     {
         lastChangeOfFace = Time.time;
 
         this.settings = settings;
-
-        random = new Random();
-
-        //SetRandomImageFilename();
-        
     }
 
     void Update()
     {
-        // This wont rly work that good since update isnt always called the same amout of times in a second. It is based on fps. For a timer is better to do smth like:
 
         if (Time.time - lastChangeOfFace > timer) // time.time being the time since the app started in miliseconds and timer being whatever you want for example 15 seconds (15000ms)
         {
             lastChangeOfFace = Time.time;
             frame++;
-            SetCycle(frame, 1); // Or SetFrame (frame)
+            SetCycle(frame, 1);
 
         }
 
     }
-
-    //void SetRandomImageFilename()
-    //{
-    //    int stringIndex = random.Next(0, settings.people.Length);
-    //    filename = people[stringIndex];
-    //}
-
 }
