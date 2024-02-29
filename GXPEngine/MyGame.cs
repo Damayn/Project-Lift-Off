d37bddd48782eb61c18c6ce26bf761e5da172b25
Dumbita <549149@student.saxion.nl>
2024-02-29 02:32:57 +0100

    Update MyGame.cs

--- GXPEngine/MyGame.cs
+++ GXPEngine/MyGame.cs
@@ -28,13 +28,25 @@ public class MyGame : Game {
     ScoreManager scoreManager;
     private bool scoreSaved;
 
+    Sound backgroundMusic;
+    SoundChannel musicSwitch;
+
     public MyGame() : base(1366, 768, false, false, -1, -1, false)
     {
         settings = new GameSettings();
+<<<<<<< Updated upstream
         scoreManager = new ScoreManager();
         this.AddChild(scoreManager);
         
         menuManager = new MenuManager(settings, this, scoreManager);
+=======
+
+        backgroundMusic = new Sound("backgroundMusic.wav",true,true);
+
+        background = new Sprite("gameplayBackground.png");
+        AddChild(background);
+        menuManager = new MenuManager(settings, this);
+>>>>>>> Stashed changes
         //kills the buttons?
         menuManager.SetMainMenu();
         AddChild(menuManager);
@@ -42,6 +54,9 @@ public class MyGame : Game {
         serialPort = new SerialPortManager ("COM3", 9600);
         readButton = new ReadButton(serialPort);
         this.AddChild(readButton);
+
+        musicSwitch = backgroundMusic.Play();
+
     }
 
     public void SetUp () 
@@ -78,6 +93,7 @@ public class MyGame : Game {
 	{
         this.targetFps = 60;
 
+<<<<<<< Updated upstream
         if (!settings.isGameOver)
         {
             if (settings.hasGameStarted && settings.hasEnteredName)
@@ -86,6 +102,17 @@ public class MyGame : Game {
                 if (!settings.isTimePaused)
                 {
                     GameOver();
+=======
+        if (settings.hasGameStarted && settings.hasEnteredName && !settings.isGameOver)
+        {
+
+            musicSwitch.Mute = false;
+
+            if (!settings.isTimePaused)
+            {
+                GameOver();
+                Pause();
+>>>>>>> Stashed changes
 
                     AddNewCustomer();
 
@@ -139,6 +166,8 @@ public class MyGame : Game {
         if (slider.currentValue <= 5) 
         {
             settings.isGameOver = true;
+            musicSwitch.Mute = true;
+
         }
     }
     
