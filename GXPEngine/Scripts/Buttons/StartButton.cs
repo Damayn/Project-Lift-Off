550d587cd268b57bdb415e42c2f1e0d14f9c768d
Dumbita <549149@student.saxion.nl>
2024-02-29 02:33:32 +0100

    Update StartButton.cs

--- GXPEngine/Scripts/Buttons/StartButton.cs
+++ GXPEngine/Scripts/Buttons/StartButton.cs
@@ -6,10 +6,31 @@ public class StartButton : Button
 {
     GameSettings settings;
 
+    Sound press;
+
     public StartButton(GameSettings settings) : base("start.png", 2, 1)
     {
 
         this.settings = settings;
 
+        press = new Sound("button_press.mp3", false, false);
+
     }
+<<<<<<< Updated upstream
+=======
+
+    protected override void Update()
+    {
+        if (hasBeenPressed)
+        {
+
+            press.Play();
+
+            settings.hasGameStarted = true;
+            settings.isGameOver = false;
+        }
+
+        base.Update();
+    }
+>>>>>>> Stashed changes
 }
